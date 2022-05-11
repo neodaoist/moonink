da0b9d7916c23c90a4c0124a16181fb54400c3e4
neodaoist <tomMobileDev123@me.me>
2022-05-11 18:48:47 -0400

    Add SVG path data for all 8 moon phases

--- test/InkRenderer.t.sol
+++ test/InkRenderer.t.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "ds-test/test.sol";
 import "forge-std/Test.sol";
 import "../src/InkRenderer.sol";
 
@@ -22,7 +21,7 @@ contract InkRendererTest is Test {
         renderer = new InkRenderer();
     }
 
-    function testOne() public {
-        assertEq(renderer.testFunction(), 1);
-    }
+    // function testOne() public {
+    //     assertEq(renderer.testFunction(), 1);
+    // }
 }
